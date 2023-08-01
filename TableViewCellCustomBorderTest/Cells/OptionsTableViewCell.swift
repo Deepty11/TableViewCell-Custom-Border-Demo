461d7d27046cd5e8f25598a85aba34b3756c96d5
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-08-01 15:23:13 +0600

    Fixed the side border issue

--- TableViewCellCustomBorderTest/Cells/OptionsTableViewCell.swift
+++ TableViewCellCustomBorderTest/Cells/OptionsTableViewCell.swift
@@ -8,21 +8,21 @@
 import UIKit
 
 class OptionsTableViewCell: UITableViewCell {
-    var leftBorder: CALayer?
-    var rightBorder: CALayer?
-    
-    var edges: UIRectEdge?
-    let borderWidth: CGFloat = 3
-    
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var container: BorderedView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         selectionStyle = .none
-        updateBorders()
     }
+}
+
+class BorderedView: UIView {
+    var leftBorder: CALayer?
+    var rightBorder: CALayer?
+    
+    let borderWidth: CGFloat = 3
     
     override var bounds: CGRect {
         didSet {
@@ -30,45 +30,36 @@ class OptionsTableViewCell: UITableViewCell {
         }
     }
     
-    func updateBorders() {
-        if leftBorder == nil { leftBorder = addBorder() }
+    func addBorder() -> CALayer {
+        let border = CALayer()
+        border.backgroundColor = UIColor.red.cgColor
+        layer.addSublayer(border)
         
-        if rightBorder == nil { rightBorder = addBorder()}
+        return border
+    }
+    
+    func updateBorders() {
+        if leftBorder == nil {
+            leftBorder = addBorder()
+        }
+        
+        if rightBorder == nil {
+            rightBorder = addBorder()
+        }
         
         leftBorder?.frame = CGRect(
-            x: container.bounds.minX,
+            x: bounds.minX,
             y: 0,
             width: borderWidth,
             height: bounds.maxY
         )
         
         rightBorder?.frame = CGRect(
-            x: container.bounds.maxX - borderWidth,
+            x: bounds.maxX - borderWidth,
             y: 0,
             width: borderWidth,
             height: bounds.maxY
         )
-        
-        // For DEBUG
-        print("bounds.minX \(bounds.minX)")
-        print("container.bounds.maxX \(container.bounds.minX)")
-
-        print("bounds.maxX \(bounds.maxX)")
-        print("container.bounds.maxX \(container.bounds.maxX)")
-    }
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        updateBorders()
-    }
-
-    func addBorder() -> CALayer {
-        let border = CALayer()
-        border.backgroundColor = UIColor.red.cgColor
-        container.layer.addSublayer(border)
-        
-        return border
     }
 }
-
 
